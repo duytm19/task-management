a486306b9d5b70f2a58ee052d413e1779581b3c5
duytm19 <tranminhduy1912@gmail.com>
2025-03-08 17:02:35 +0700

    filter by status

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
 const express= require("express")
 require("dotenv").config()
 const database = require("./config/database")
-const Task = require("./models/task.model")
+
 
 database.connect()
 const app = express()
 const port = 3000
+const Router = require("./api/v1/routes/index.route")
 
-app.get("/tasks", async (req,res)=>{
-    const tasks = await Task.find({
-        deleted:false
-    })
-    res.json(tasks)
-})
-app.get("/tasks/detail/:id", async (req,res)=>{
-    const id= req.params.id
-
-    const tasks = await Task.find({
-        _id:id,
-        deleted:false
-    })
-    res.json(task)
-})
-app.listen(port,()=>{
-    console.log("App listening on port 3000")
-})
+Router(app)
+app.listen(port, () => {
+    console.log(`App is running at http://localhost:${port}`);
+  });
+  

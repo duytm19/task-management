bae587ec01302b5e675ab81cc4baad4eb47e6943
duytm19 <tranminhduy1912@gmail.com>
2025-03-09 13:03:35 +0700

    change-multi in task

--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -8,4 +8,8 @@ router.get("/",controller.index)
 router.get("/detail/:id",controller.detail)
 
 router.patch("/change-status/:id",controller.changeStatus)
+
+router.patch("/change-multi",controller.changeMulti)
+
+router.post("/create",controller.createPost)
 module.exports=router

23785d87a8a7398f459dce9f2a6fb91d15205dfe
duytm19 <tranminhduy1912@gmail.com>
2025-03-11 17:09:23 +0700

    get list user

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -170,3 +170,13 @@ module.exports.detail = async (req,res) =>{
         info:user
     })
 }
+// [GET] /api/v1/users/list
+module.exports.list = async (req,res) =>{
+    const users = await User.find({deleted:false}).select("fullName email")
+
+    res.json({
+        code:200,
+        message:"Successfully",
+        users:users
+    })
+}

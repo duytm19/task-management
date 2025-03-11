47328ba98c38403bc83e4a3c491ea6e755e022ca
duytm19 <tranminhduy1912@gmail.com>
2025-03-11 14:41:41 +0700

    send otp email

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -1,6 +1,8 @@
 const md5 = require("md5")
 const User = require("../models/user.model")
-
+const ForgotPassword = require("../models/forgot-password.models")
+const generateHelper = require("../../../helpers/generate")
+const sendMailHelper = require("../../../helpers/sendEmail")
 // [POST] /api/v1/users/register
 module.exports.register = async (req,res)=>{
     req.body.password =  md5(req.body.password)
@@ -34,7 +36,7 @@ module.exports.register = async (req,res)=>{
 
 }
 
-// [GET] /api/v1/users/login
+// [POST] /api/v1/users/login
 module.exports.login = async(req,res)=>{
     const email= req.body.email
     const password = req.body.password
@@ -59,11 +61,56 @@ module.exports.login = async(req,res)=>{
         })
         return
     }
-    const token =user.token
+    const token =user.tokenUser
     res.cookie("token",token)
     res.json({
         code:200,
         message:"Login successfully!",
         token:token
+    })
+}
+
+// [POST] /api/v1/users/password/forgot
+
+module.exports.forgotPassword =async(req,res)=>{
+    const email = req.body.email
+
+    const user = await User.findOne({
+        email:email,
+        deleted: false
+    })
+
+    if(!email){
+        res.json({
+            code:400,
+            message:"Account is not existence !!!"
+        })
+
+        return
+    }
+
+    const otp = generateHelper.generateRandomNumber(8)
+
+    const objectForgotPassword ={
+        email: email,
+        otp:otp,
+        expireAt:Date.now()
+    }
+
+    const forgotPassword = new ForgotPassword(objectForgotPassword)
+
+    await forgotPassword.save()
+
+    //Send OTP
+    const subject = "OTP CODE TO RESET YOUR PASSWORD"
+    const html = `<h3> OTP CODE TO RESET YOUR PASSWORD: </h3>
+    <b style="color: red;">${otp}</b>.<h3>The expired time is: 3 minutes</h3>
+    `
+    sendMailHelper.sendMail(email,subject,html)
+    //End Send OTP
+
+    res.json({
+        code:200,
+        message:'Send otp successfull, please check your email'
     })
 }

8d57d4ac580ceefd91bdedbfbd24baf19868f5ff
duytm19 <tranminhduy1912@gmail.com>
2025-03-11 13:31:20 +0700

    register new user

--- api/v1/routes/index.route.js
+++ api/v1/routes/index.route.js
@@ -1,5 +1,7 @@
 const taskRoutes = require("./task.route")
-
+const userRoutes = require('./user.route')
 module.exports = (app) => {
     app.use("/api/v1/tasks",taskRoutes)
+
+    app.use("/api/v1/users",userRoutes)
 }
--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express= require("express")
 require("dotenv").config()
 const database = require("./config/database")
 const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
 const cors = require("cors")
 database.connect()
 const app = express()
 const port = 3000
 const Router = require("./api/v1/routes/index.route")
 app.use(cors())
+
+app.use(cookieParser())
 // parse application/json
 app.use(bodyParser.json())
 
